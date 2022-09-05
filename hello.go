0b593aa1ca89335783f5f2d0f516e2ed8b790211
Scott Hunter <scott@tacomadata.com>
2022-09-05 16:09:47 -0700

    Added 99 bottles of beer

--- hello.go
+++ hello.go
@@ -46,19 +46,33 @@ func myfunc2(loopcount int) {
 	fmt.Printf("Address is %p\n", &c)
 }
 
+func singBottles(b int) {
+	thisbeer := b
+	nextbeer := b
+	nextbeer -= 1
+
+	if b > 1 {
+		fmt.Println(strconv.Itoa(thisbeer) + " bottles of beer on the wall " + strconv.Itoa(thisbeer) + " bottles of beer")
+		fmt.Println("take 1 down, pass it around, " + strconv.Itoa(nextbeer) + "bottles of beer on the wall!")
+	} else {
+		fmt.Println("1 bottle of beer on the wall, only 1 bottle of beer... Buurrrp!")
+	}
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 
 	a := 1
-	b := 1
-	c := 1
 
 	fmt.Printf("Address is %p\n", &a)
-	fmt.Printf("Address is %p\n", &b)
-	fmt.Printf("Address is %p\n", &c)
+
 	for i := 0; i < 10; i += 1 {
 		myfunc2(i)
+	}
+
+	for b := 99; b > 0; b-- {
+		singBottles(b)
 	}
 
 }
